73c58f9ca422a3d7053ad3d8a5a19c5843b40967
Kimi <stu207503@mail.uni-kiel.de>
2022-12-04 11:50:12 +0100

    day 4

--- Test.py
+++ Test.py
@@ -1,10 +1,11 @@
-import Day03
+import Day04
 import convert
 
 # get the input
-my_file = open('C:/Users/Kimi/Documents/Advent of code/03input.txt', 'r')
+my_file = open('C:/Users/Kimi/Documents/Advent of code/04input.txt', 'r')
 
 # try code for day 3
-L = convert.get_lists(my_file)
+L = convert.get_lis(my_file)
 print(L)
-print(Day03.get_groups(L))
+print(Day04.get_total_overlaps(L))
+
