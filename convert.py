256788eee21c99a8b6355329b57e32b1319f5431
Kimi <stu207503@mail.uni-kiel.de>
2022-12-04 11:49:59 +0100

    add docstrings and list for day 4

--- convert.py
+++ convert.py
@@ -1,4 +1,9 @@
 def get_list(my_file):
+    """
+       get a list for Day 1
+       :param my_file: the strings to convert into a list
+       :return: a list
+    """
     L = []
     for line in my_file.readlines():
         my_var = line.strip()
@@ -9,8 +14,31 @@ def get_list(my_file):
 
 
 def get_lists(my_file):
+    """
+    get a list for Day 2 and 3
+    :param my_file: the strings to convert into a list
+    :return: a list
+    """
     L = []
     for line in my_file.readlines():
         my_var = line.strip()
         L += [my_var]
     return L
+
+
+def get_lis(my_file):
+    """
+    get a list for Day 4
+    :param my_file: the strings to convert into a list
+    :return: a list
+    """
+    L = []
+    for line in my_file.readlines():
+        my_var = line.strip()
+        L += [my_var]
+
+    LL = []
+    for nums in L:
+        x = nums.split(',')
+        LL += x
+    return LL
