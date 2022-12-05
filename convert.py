eb8cc3693aeb77069b0014731ca8dcec8c0f2277
Kimi <stu207503@mail.uni-kiel.de>
2022-12-05 17:46:50 +0100

    parse input of day5

--- convert.py
+++ convert.py
@@ -1,3 +1,6 @@
+import re
+
+
 def get_list(my_file):
     """
        get a list for Day 1
@@ -42,3 +45,30 @@ def get_lis(my_file):
         x = nums.split(',')
         LL += x
     return LL
+
+
+def lists_for_moves(my_file):
+    """
+    get a list for day 5
+    :param my_file:
+    :return:
+    """
+    stop = False
+    cargo = []
+    L = []
+    for line in my_file.readlines():
+        if line[0] != '\n' and line[1].isdigit():
+            stop = True
+        elif stop and line[0] == 'm':
+            direction = []
+            moves = line.strip().split(' ')
+            for i in range(0, len(moves)):
+                if moves[i].isdigit():
+                    direction += [int(moves[i])]
+            L += [direction]
+        else:
+            my_list = [line[idx:idx + 4].strip() for idx in range(0, len(line), 4)]
+            cargo += [my_list]
+    return cargo, L
+
+
