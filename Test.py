2a6f85998650b18b190b7d7136896ed93968d25c
Kimi <stu207503@mail.uni-kiel.de>
2022-12-06 16:45:41 +0100

    execute for day 6

--- Test.py
+++ Test.py
@@ -1,14 +1,12 @@
-import Day05
+import Day06
 import convert
 
 # get the input
-my_file = open('C:/Users/Kimi/Documents/Advent of code/05input.txt', 'r')
+with open('C:/Users/Kimi/Documents/Advent of code/06input.txt', 'r') as file:
+    data = file.read().replace('\n', '')
 
-# try code for day 3
-cargo, L = convert.lists_for_moves(my_file)
-print(L, cargo)
-res = Day05.rearrange_part2(cargo, L)
+# try code for day 6
+
+res = Day06.get_start(data)
 print(res)
 
-for i in res:
-    print(i[-1])
