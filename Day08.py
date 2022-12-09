da91063c285ee127c1e1d9d536627f708c284872
Kimi <stu207503@mail.uni-kiel.de>
2022-12-09 18:24:32 +0100

    Simplify the code

--- Day08.py
+++ Day08.py
@@ -1,4 +1,9 @@
 def get_visible(heights):
+    """
+    Get the number of visible trees -> part 1 of day 8
+    :param heights: all the heights of trees in a multi-dimensional list
+    :return: the number of visible trees
+    """
     columns = len(heights[0])
     rows = len(heights)
     visible = 2 * columns + 2 * rows - 4
@@ -13,27 +18,17 @@ def get_visible(heights):
 
 
 def shortest(heights, i, j, direction, tree):
-    match direction:
-        case "up":
-            m = (i, 1)
-            n = [heights, i-1, j]
-            o = heights[i-1][j]
-        case "down":
-            m = (i, len(heights) - 2)
-            n = [heights, i + 1, j]
-            o = heights[i + 1][j]
-        case "left":
-            m = (1, j)
-            n = [heights, i, j - 1]
-            o = heights[i][j - 1]
-        case "right":
-            m = (len(heights[0]) - 2, j)
-            n = [heights, i, j + 1]
-            o = heights[i][j + 1]
-        case _:
-            m = 0
-            n = 0
-            o = 0
+    """
+    Check if the tree is the shortest in it a given direction
+    :param heights: all the heights of trees in a multi-dimensional list
+    :param i: row of the tree
+    :param j: column of the tree
+    :param direction: the direction to check
+    :param tree: the height of the tree
+    :return: True if the tree is the shortest in the given direction
+    """
+    # get the variables needed for the recursive function
+    m, n, o = get_variables(heights, i, j, direction)
 
     if m[1] == m[0] and o < tree:
         return True
@@ -44,6 +39,11 @@ def shortest(heights, i, j, direction, tree):
 
 
 def scenic_score(heights):
+    """
+    Get the scenic score -> part 2 of day 8
+    :param heights: all the heights of trees in a multi-dimensional list
+    :return: the scenic score
+    """
     columns = len(heights[0])
     rows = len(heights)
     part_score = 0
@@ -51,7 +51,8 @@ def scenic_score(heights):
     # iterate over the trees in the middle
     for i in range(1, rows - 1):
         for j in range(1, columns - 1):
-            temp = view(heights, i, j, "up", heights[i][j],part_score) * view(heights, i, j, "down", heights[i][j], part_score) * \
+            # get the score for each tree and see if it is the highest
+            temp = view(heights, i, j, "up", heights[i][j], part_score) * view(heights, i, j, "down", heights[i][j], part_score) * \
                     view(heights, i, j, "left", heights[i][j], part_score) * view(heights, i, j, "right", heights[i][j], part_score)
             if temp > score:
                 score = temp
@@ -59,37 +60,44 @@ def scenic_score(heights):
 
 
 def view(heights, i, j, direction, tree, part_score):
-    match direction:
-        case "up":
-            m = (i, 1)
-            n = [heights, i-1, j]
-            o = heights[i-1][j]
-        case "down":
-            m = (i, len(heights) - 2)
-            n = [heights, i + 1, j]
-            o = heights[i + 1][j]
-        case "left":
-            m = (1, j)
-            n = [heights, i, j - 1]
-            o = heights[i][j - 1]
-        case "right":
-            m = (len(heights[0]) - 2, j)
-            n = [heights, i, j + 1]
-            o = heights[i][j + 1]
-        case _:
-            m = 0
-            n = 0
-            o = 0
-
-    if m[1] == m[0] and o < tree:
-        part_score += 1
-        return part_score
-    if o == tree:
-        part_score += 1
-        return part_score
-    if o > tree:
-        part_score += 1
-        return part_score
-    else:
+    """
+    Get the score for a given tree
+    :param heights: all the heights of trees in a multi-dimensional list
+    :param i: row of the tree
+    :param j: column of the tree
+    :param direction: the direction to check
+    :param tree: the height of the tree
+    :param part_score: the score of the tree
+    :return: the score of the tree
+    """
+    # get the variables needed for the recursive function
+    m, n, o = get_variables(heights, i, j, direction)
+    # if the tree is smaller than the next one, keep counting
+    if o < tree and m[1] != m[0]:
         part_score += 1
         return view(n[0], n[1], n[2], direction, tree, part_score)
+    else:
+        part_score += 1
+        return part_score
+
+
+def get_variables(heights, i, j, direction):
+    """
+    Get the variables needed for the recursive function
+    :param heights: all the heights of trees in a multi-dimensional list
+    :param i: row of the tree
+    :param j: column of the tree
+    :param direction: the direction to check
+    :return: the variables needed for the recursive function
+    """
+    match direction:
+        case "up":
+            return (i, 1), [heights, i - 1, j], heights[i - 1][j]
+        case "down":
+            return (i, len(heights) - 2), [heights, i + 1, j], heights[i + 1][j]
+        case "left":
+            return (1, j), [heights, i, j - 1], heights[i][j - 1]
+        case "right":
+            return (len(heights[0]) - 2, j), [heights, i, j + 1], heights[i][j + 1]
+        case _:
+            return 0, 0, 0
